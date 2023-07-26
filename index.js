1dfd48132748b7bd04a8ac94d0baaeb56e693e2c
SilentSpider <84772129+SilentSpider049@users.noreply.github.com>
2023-07-26 14:44:56 +0100

    update

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function msg() {
     let message = document.querySelector("#message")
     message.innerHTML = "Welcome, " + userInput.value + "!"
 }
+
+let showGif = document.getElementById("showGif")
+let showImage = document.getElementById("showImage")
+showGif.addEventListener("click", function() {
+    showImage.style.display = "block"
+    showImage.style.textAlign = "center"
+})
